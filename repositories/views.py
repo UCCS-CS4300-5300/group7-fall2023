e96a34183152517ba61ccc30c012b61bd0324b62
Brynn Robenstein <brobenst@uccs.edu>
2023-11-10 21:18:09 +0000

    refactored templates to inherit from a base template and made home template show actual repository data

--- repositories/views.py
+++ repositories/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from repositories.models import Repository
 
 
 class HomePageView(TemplateView):
@@ -7,19 +8,22 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["repositories"] = Repository.objects.all().order_by(
+            '-stars')[:10]
         return context
-      
-class AccountPageView(TemplateView):
-  template_name = "account.html"
 
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      return context
+
+class AccountPageView(TemplateView):
+    template_name = "account.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 class ExplorePageView(TemplateView):
-  template_name = "explore.html"
+    template_name = "explore.html"
 
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
