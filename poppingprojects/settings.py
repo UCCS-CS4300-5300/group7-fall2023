23db4315f42bb6d5be8d3b87ea7f994f8fde2b0c
Brynn Robenstein <brobenst@uccs.edu>
2023-11-15 00:29:45 +0000

    Added tests for views, models, and updater

--- poppingprojects/settings.py
+++ poppingprojects/settings.py
@@ -22,12 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-if SECRET_KEY is None:
-    print(
-        "Please setup a SECRET_KEY in the Secrets (Environment variables) tab. See README.md for more."
-    )
-    exit(1)
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- repositories/updater.py
+++ repositories/updater.py
@@ -21,7 +21,7 @@ HEADERS = {
 # Query for api post request
 QUERY = '''
 query {
-  search(query: "sort:stars", type: REPOSITORY, first:100 %s) {
+  search(query: "stars:>5000 sort:stars", type: REPOSITORY, first:100 %s) {
     repositories: edges {
       cursor
       info: node {
@@ -73,13 +73,13 @@ def get_repositories(total):
     return repositories
 
 
-def update():
+def update(total):
     '''
     Parse data from a list of repositories in JSON format and add the data
     to the database. If the repository already existts, the data for it will
     be updated. If it doesn't exist, a new one will be created.
     '''
-    repositories = get_repositories(TOTAL_REPOSITORIES)
+    repositories = get_repositories(total)
 
     for repository in repositories:
 
@@ -115,10 +115,10 @@ def update():
             url=url, defaults=defaults)
 
 
-def start():
+def start(total):
     '''
     Schedule task to update repository database every hour
     '''
     scheduler = BackgroundScheduler({'apscheduler.timezone': 'UTC'})
-    scheduler.add_job(update, 'interval', hours=1)
+    scheduler.add_job(update, 'interval', args=(total,), hours=1)
     scheduler.start()
