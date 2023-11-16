71d059f20b942f9c68ae366df2b253ccb31bab0c
Brynn Robenstein <brobenst@uccs.edu>
2023-11-16 22:44:18 +0000

    Added basic explore page UI

--- repositories/views.py
+++ repositories/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from .models import Repository
+from .models import Repository, Language
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
@@ -27,7 +27,9 @@ class ExplorePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
       context = super().get_context_data(**kwargs)
-      context["repositories"] = Repository.objects.all()
+      context["repositories"] = Repository.objects.all().order_by(
+        '-last_commit')[:10]
+      context["languages"] = Language.objects.all()
       return context
 
 def login(request):
